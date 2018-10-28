3895e6d7961b917c293628899ea930f1ca6b97a8
Roy Fox <roy.d.fox@gmail.com>
2018-10-27 20:28:01 -0700

    dicttree

--- tdict.py
+++ tdict.py
@@ -3,20 +3,20 @@ from collections import abc
 from typing import Mapping
 
 
-class DictTree(abc.Mapping):
+class Tdict(abc.MutableMapping):
     """
-    Nested dict.
+    Tree dict.
 
     Values of `str` keys can be accessed either as attributes or as items.
-    Values of sub-trees can be deep-accessed by `list` keys.
+    Values of `tuple` keys are stored in nested `Tdict`s.
 
-    >>> d = DictTree(
-            {'a': 1, ('non', 'str', 'key'): 'allowed'},
-            json.loads('{"sub": {"x": 10} }'),
+    >>> d = Tdict(
+            {'a': 1, 'sub': {tuple('non-str keys allowed'.split()): True}},
+            json.loads('{"sub": {"x": 10}}'),
             b=2, **{'c': 3}, d=4)
     >>> d
-    DictTree(a=1, ('non', 'str', 'key')='allowed', sub=DictTree(x=10), b=2, c=3, d=4)
-    >>> assert d[[]] is d
+    Tdict(a=1, sub=Tdict(('non-str', 'keys', 'allowed')=True, x=10), b=2, c=3, d=4)
+    >>> assert d[()] is d
     >>> d.a
     1
     >>> d['b']
@@ -25,32 +25,20 @@ class DictTree(abc.Mapping):
     >>> d['f'] = 6
     >>> 'sub' in d
     True
-    >>> ['sub', 'x'] in d
+    >>> ('sub', 'x') in d
     True
     >>> d.sub.x
     10
     >>> d.sub.y = 11
-    >>> d[['sub', 'y']]
+    >>> d['sub', 'y']
     11
     >>> d |= {'g': 7}
     >>> d += {'a': 100}
-    >>> d * DictTree(sub=DictTree(y=7))
-    DictTree(a=101, ('non', 'str', 'key')='allowed', sub=DictTree(x=10, y=77), b=2, c=3, d=4, e=5, f=6, g=7)
-    >>> list(d.items())
-    [(['a'], 101),
-     ([('non', 'str', 'key')], 'allowed'),
-     (['sub', 'x'], 10),
-     (['sub', 'y'], 11),
-     (['b'], 2),
-     (['c'], 3),
-     (['d'], 4),
-     (['e'], 5),
-     (['f'], 6),
-     (['g'], 7)]
-    >>> list(d.items(False))
+    >>> d * Tdict(sub=Tdict(y=7)) + dict(sub=100)
+    Tdict(a=101, sub=Tdict(('non-str', 'keys', 'allowed')=101, x=110, y=177), b=2, c=3, d=4, e=5, f=6, g=7)
+    >>> list(d.items(deep=False))
     [('a', 101),
-     (('non', 'str', 'key'), 'allowed'),
-     ('sub', DictTree(x=10, y=11)),
+     ('sub', Tdict(('non-str', 'keys', 'allowed')=True, x=10, y=11)),
      ('b', 2),
      ('c', 3),
      ('d', 4),
@@ -58,7 +46,7 @@ class DictTree(abc.Mapping):
      ('f', 6),
      ('g', 7)]
     >>> len(d)
-    10
+    8
 
     @DynamicAttrs
     """
@@ -68,102 +56,79 @@ class DictTree(abc.Mapping):
 
         Args:
             *maps (Mapping): Update attributes from these `Mapping`s.
-                             Values that are themselves `Mapping`s are deep-copied as sub-`DictTree`s.
+                             Values that are themselves `Mapping`s are deep-copied as sub-`Tdict`s.
             **attr: Extra attributes (not copied).
         """
         super().__init__()
         for m in maps:
-            for k, v in DictTree._shallow_items(m):
+            for k, v in Tdict._shallow_items(m):
                 if isinstance(v, abc.Mapping):
-                    self[k] = DictTree(v)
+                    vars(self).setdefault(k, Tdict()).update(v)
                 else:
-                    self[k] = v
+                    vars(self)[k] = v
         self.update(attr)
 
     def __getitem__(self, k):
         """
 
         Args:
-            k: Item key. May be a `list` for deep access.
+            k: Item key. May be a `tuple` for deep access.
 
         Returns:
             Item value.
         """
-        if isinstance(k, list):
+        if isinstance(k, tuple):
             if len(k) == 0:
                 return self
             elif len(k) == 1:
-                return self[k[0]]
+                return vars(self)[k[0]]
             else:
-                return self[k[0]][k[1:]]
+                return vars(self)[k[0]][k[1:]]
         else:
             return vars(self)[k]
 
-    def setdefault(self, k, v):
-        """
-
-        Args:
-            k: Item key. May be a `list` for deep access, which creates nested `DictTree`s as needed.
-            v: Item value. This value is set if the item doesn't exist.
-
-        Returns:
-            Item value.
-
-        Raises:
-            KeyError: Item key is reserved.
-        """
-        if isinstance(k, list):
-            if len(k) == 0:
-                return self
-            elif len(k) == 1:
-                return self.setdefault(k[0], v)
-            else:
-                return self.setdefault(k[0], DictTree()).setdefault(k[1:], v)
-        elif k in RESERVED:
-            raise KeyError(f'key "{k}" is reserved')
-        else:
-            return vars(self).setdefault(k, v)
+    __getattr__ = __getitem__
 
     def __setitem__(self, k, v):
         """
 
         Args:
-            k: Item key. May be a `list` for deep access, which creates nested `DictTree`s as needed.
+            k: Item key. May be a `tuple` for deep access, which creates nested `Tdict`s as needed.
             v: Item value.
 
         Raises:
-            KeyError: Item key is an empty `list` or reserved.
+            KeyError: Item key is an empty `tuple`.
         """
-        if isinstance(k, list):
+        if isinstance(k, tuple):
             if len(k) == 0:
                 raise KeyError("cannot assign to root")
             elif len(k) == 1:
-                self[k[0]] = v
+                vars(self)[k[0]] = v
             else:
-                self.setdefault(k[0], DictTree())[k[1:]] = v
-        elif k in RESERVED:
-            raise KeyError(f'key "{k}" is reserved')
+                vars(self).setdefault(k[0], Tdict())[k[1:]] = v
         else:
             vars(self)[k] = v
 
-    def __setattr__(self, k, v):
-        self[k] = v
+    __setattr__ = __setitem__
 
     def __delitem__(self, k):
         """
 
         Args:
-            k: Item key. May be a `list` for deep access.
+            k: Item key. May be a `tuple` for deep access.
+
+        Raises:
+            KeyError: Item key is an empty `tuple`.
         """
-        if isinstance(k, list):
+        if isinstance(k, tuple):
             if len(k) == 0:
                 raise KeyError("cannot delete root")
             elif len(k) == 1:
-                self.__delitem__(k[0])
+                del vars(self)[k[0]]
             else:
-                self[k[0]].__delitem__(k[1:])
+                del vars(self)[k[0]][k[1:]]
         else:
-            vars(self).__delitem__(k)
+            del vars(self)[k]
 
     def keys(self, deep=True):
         """
@@ -176,11 +141,11 @@ class DictTree(abc.Mapping):
         """
         if deep:
             for k, v in vars(self).items():
-                if isinstance(v, DictTree):
+                if isinstance(v, Tdict):
                     for k_ in v.keys():
-                        yield [k] + k_
+                        yield (k,) + k_
                 else:
-                    yield [k]
+                    yield (k,)
         else:
             yield from vars(self).keys()
 
@@ -195,11 +160,11 @@ class DictTree(abc.Mapping):
         """
         if deep:
             for k, v in vars(self).items():
-                if isinstance(v, DictTree):
+                if isinstance(v, Tdict):
                     for k_, v_ in v.items():
-                        yield [k] + k_, v_
+                        yield (k,) + k_, v_
                 else:
-                    yield [k], v
+                    yield (k,), v
         else:
             yield from vars(self).items()
 
@@ -214,7 +179,7 @@ class DictTree(abc.Mapping):
         """
         if deep:
             for v in vars(self).values():
-                if isinstance(v, DictTree):
+                if isinstance(v, Tdict):
                     yield from v.values()
                 else:
                     yield v
@@ -233,30 +198,30 @@ class DictTree(abc.Mapping):
         """
 
         Returns:
-            int: Number of leaf (non-`DictTree`) values.
+            int: Number of leaf (non-`Tdict`) values.
         """
-        return sum(len(v) if isinstance(v, DictTree) else 1 for v in self.values())
+        return sum(len(v) if isinstance(v, Tdict) else 1 for v in self.values())
 
     def __repr__(self):
-        return f'DictTree({", ".join(f"{k}={v!r}" for k, v in vars(self).items())})'
+        return f'Tdict({", ".join(f"{k}={v!r}" for k, v in vars(self).items())})'
 
     def __contains__(self, k):
         """
 
         Args:
-            k: Item key. May be a `list` for deep access.
+            k: Item key. May be a `tuple` for deep access.
 
         Returns:
             bool: Key existence.
         """
-        if isinstance(k, list):
+        if isinstance(k, tuple):
             if len(k) == 0:
                 return True
             elif len(k) == 1:
                 return k[0] in vars(self)
             else:
                 if k[0] in vars(self):
-                    return k[1:] in self[k[0]]
+                    return k[1:] in vars(self)[k[0]]
                 else:
                     return False
         else:
@@ -266,14 +231,14 @@ class DictTree(abc.Mapping):
         """
 
         Returns:
-            DictTree: Deep copy of `self`.
+            Tdict: Deep copy of `self`.
         """
-        res = DictTree()
-        for k, v in self.items():
-            if isinstance(v, DictTree):
-                res[k] = v.copy()
+        res = Tdict()
+        for k, v in self.items(False):
+            if isinstance(v, Tdict):
+                vars(res)[k] = v.copy()
             else:
-                res[k] = v
+                vars(res)[k] = v
         return res
 
     def update(self, d, o=None):
@@ -289,31 +254,36 @@ class DictTree(abc.Mapping):
                 Default: replace current value, `x = y`.
 
         Returns:
-            DictTree: `self` after update.
+            Tdict: `self` after update.
         """
         if isinstance(d, abc.Mapping):
-            for k, v in DictTree._shallow_items(d):
+            for k, v in Tdict._shallow_items(d):
                 if k in vars(self):
-                    if isinstance(self[k], DictTree):
+                    v_ = vars(self)[k]
+                    if isinstance(v_, Tdict):
                         if isinstance(v, abc.Mapping) or o is not None:
-                            self[k].update(v, o)
+                            vars(self)[k].update(v, o)
                         else:
-                            self[k] = v
+                            vars(self)[k] = v
+                    elif o is None:
+                        vars(self)[k] = v
                     else:
-                        self[k] = o(self[k], v)
+                        vars(self)[k] = o(v_, v)
                 else:
-                    self[k] = v
+                    vars(self)[k] = v
         else:
             for k, v in vars(self).items():
-                if o is None:
-                    self[k] = d
+                if isinstance(v, Tdict):
+                    v.update(d, o)
+                elif o is None:
+                    vars(self)[k] = d
                 else:
-                    self[k] = o(self[k], d)
+                    vars(self)[k] = o(v, d)
         return self
 
     @staticmethod
     def _shallow_items(m):
-        if isinstance(m, DictTree):
+        if isinstance(m, Tdict):
             return m.items(False)
         else:
             return m.items()
@@ -348,7 +318,5 @@ OPERATORS = {
 }
 
 for name, op in OPERATORS.items():
-    setattr(DictTree, f'__{name}__', Op(op, False))
-    setattr(DictTree, f'__i{name}__', Op(op, True))
-
-RESERVED = set(dir(DictTree()))
+    setattr(Tdict, f'__{name}__', Op(op, False))
+    setattr(Tdict, f'__i{name}__', Op(op, True))
