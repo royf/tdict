67b8ea1b8575b57b63fd1f74f419bb4678e21dad
Roy Fox <roy.d.fox@gmail.com>
2019-01-01 15:13:30 -0800

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tdict',
-    version='0.1',
+    version='0.1.1',
     packages=['tdict'],
     url='https://github.com/royf/tdict',
     license='MIT',
