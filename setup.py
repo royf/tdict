7ebd4ca6cca9099189c3716e468a5f676d766077
Roy Fox <roy.d.fox@gmail.com>
2026-02-04 13:09:34 -0800

    setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='tdict',
-    version='0.4.6.1',
+    version='0.5',
     packages=['tdict'],
     url='https://github.com/royf/tdict',
     license='MIT',
     author='Roy Fox',
     author_email='roy.d.fox@gmail.com',
-    description='Tree-structured dict for easy handling of configurations and results'
+    description='`Tdict` is a tree-structured mapping for easy handling of configurations and results'
 )
